deb05d0a5d2322e5545d338cf719f67d6ace61b1
gg-mg <ggichuki2017@gmail.com>
2020-02-01 01:16:55 -0600

    Changed the main.css aside nav ul li a code so that navigation is on click on any part of the button

--- SSP02.php
+++ SSP02.php
@@ -1,9 +1,9 @@
 
 
 
-  <?php include "PHP/include_header.php";?>
-  <?php include "PHP/include_leftNav.php";?>
-  <?php include "PHP/include_topNav.php";?>
+    <?php include "PHP/include_header.php";?>
+    <?php include "PHP/include_leftNav.php";?>
+    <?php include "PHP/include_topNav.php";?>
   
   
 
@@ -14,13 +14,13 @@
 
         <?php include "PHP/include_dynamic_content.php";?>
 
-    <!-- Professor Hyde instructions require the student to type <p>Hello World (HTML)</p> -->
+    
     </main>
     <footer>
         <hr>
         Dislaimer: Oklahoma City Community College does not necessarily
         endorse the content or the respective links on this web page.
-        <?php include "PHP/include_footer.php";?>
+     <?php include "PHP/include_footer.php";?>
     </footer>
     
 </body>
